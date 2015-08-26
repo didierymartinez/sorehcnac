837fe571eee6a95b097425d951c789e67c162ead
reidid <didierymartinez@gmail.com>
2015-08-25 22:49:46 -0500

    Login facebook
    Login facebook

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+	'table' => 'oauth_identities',
+	'providers' => [
+		'facebook' => [
+			'client_id' => '232000390243356',
+			'client_secret' => '9d963f8a1fd4f5e4c43d4ec61f0a6adf',
+			'redirect_uri' => 'http://localhost/sorehcnac/public/login',
+			'scope' => [],
+		],
+		'google' => [
+			'client_id' => '12345678',
+			'client_secret' => 'y0ur53cr374ppk3y',
+			'redirect_uri' => 'https://example.com/your/google/redirect',
+			'scope' => [],
+		],
+		'github' => [
+			'client_id' => '12345678',
+			'client_secret' => 'y0ur53cr374ppk3y',
+			'redirect_uri' => 'https://example.com/your/github/redirect',
+			'scope' => [],
+		],
+		'linkedin' => [
+			'client_id' => '12345678',
+			'client_secret' => 'y0ur53cr374ppk3y',
+			'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+			'scope' => [],
+		],
+		'instagram' => [
+			'client_id' => '12345678',
+			'client_secret' => 'y0ur53cr374ppk3y',
+			'redirect_uri' => 'https://example.com/your/instagram/redirect',
+			'scope' => [],
+		],
+		'soundcloud' => [
+			'client_id' => '12345678',
+			'client_secret' => 'y0ur53cr374ppk3y',
+			'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+			'scope' => [],
+		],
+	],
+];
